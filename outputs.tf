7a0dda3cec24f121bf5d6aebc622ada91dc78cd1
Kislaya Srivastava <Kislaya.srivastava@gmail.com>
2023-10-03 20:42:14 +0000

    #35 Invalidated Cloudfront stack when content version changed.

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "S3_website_endpoint" {
     description = "Static Website Endpoint Url"
     value=module.terrahouse_aws.website_endpoint
 }
+
+output "cloudfront_url" {
+  description = "Cloudfront Distribution URL"
+  value=module.terrahouse_aws.cloudfront_url
+}
